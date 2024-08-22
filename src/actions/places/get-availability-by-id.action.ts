df02d13e9c8fc20ebccaec9978248c3c82db2992
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-22 15:08:57 -0400

    chore: Add prerender flag to index page

---
+++ src/actions/places/get-availability-by-id.action.ts
@@ -0,0 +1,27 @@
+import { defineAction, z } from 'astro:actions';
+
+export const getAvailabilityById = defineAction({
+  accept: 'json',
+  input: z.string(),
+  handler: async (placeId) => {
+    await new Promise((resolve) => setTimeout(() => resolve, 3000));
+
+    const isAvailable = Math.random() > 0.5; // true | false
+    const spotsAvailable = Math.floor(Math.random() * 10) + 1; // 1-11
+
+    let message = 'No hay espacios disponibles';
+    if (isAvailable) {
+      message =
+        spotsAvailable > 1
+          ? `${spotsAvailable} espacios disponible`
+          : `1 espacio disponible`;
+    }
+
+    return {
+      id: placeId,
+      isAvailable: isAvailable,
+      spotsAvailable: spotsAvailable,
+      message: message,
+    };
+  },
+});
